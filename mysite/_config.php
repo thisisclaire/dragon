d001c90441f03b9161ef30290fe3721cb73751f8
Jon Shutt <jon@futureweb.co.nz>
2014-07-22 09:04:42 +1200

    error files updated when changed template
    edited config so it connects to local and remote databases

--- mysite/_config.php
+++ mysite/_config.php
@@ -2,16 +2,43 @@
 
 global $project;
 $project = 'mysite';
-
 global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'root',
-	"password" => '',
-	"database" => 'SS_mysite',
-	"path" => '',
+
+
+$whitelist = array(
+    '127.0.0.1',
+    '::1'
 );
+
+// if on remote server
+if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+	
+   $databaseConfig = array(
+		"type" => 'MySQLDatabase',
+		"server" => 'localhost',
+		"username" => 'claire_dragonusr',
+		"password" => 'R@&1zTE-Pd_S',
+		"database" => 'claire_dragon',
+		"path" => '',
+	);
+	
+	Security::setDefaultAdmin('admin','secretadminpassword');
+	
+// else if on local server	
+} else {
+
+	$databaseConfig = array(
+		"type" => 'MySQLDatabase',
+		"server" => 'localhost',
+		"username" => 'root',
+		"password" => '',
+		"database" => 'SS_mysite',
+		"path" => '',
+	);	
+	
+	Security::setDefaultAdmin('admin','secretadminpassword');
+}
+
 
 // Set the site locale
 i18n::set_locale('en_US');
