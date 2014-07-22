bd9230c8de5b9bf43c87ef5b8b1ed4e13bebc086
Jon Shutt <jon@futureweb.co.nz>
2014-07-23 09:29:03 +1200

    adjust js so it closes the menu on resize

--- themes/dragonisland/javascript/script.js
+++ themes/dragonisland/javascript/script.js
@@ -1,9 +1,17 @@
 (function($) {    
     $(document).ready(function() {
+	 
+	 	// open close the mobile menu
 		$('#primarymobilebutton').click(function(){
-			
-			
 			$('.primarymobile').toggle();
 		})
+		
+		
+		
+		// close menu when screen resized
+		$(window).resize(function() {			
+			$('.primarymobile').hide();
+		});
+		
     });
 }(jQuery));
