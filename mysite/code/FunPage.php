6b348b1861a9fe970a26ee5368a5759604a5e461
Jon Shutt <jon@futureweb.co.nz>
2014-08-03 19:34:09 +1200

    fun things ahoy....

--- mysite/code/FunPage.php
+++ mysite/code/FunPage.php
@@ -7,14 +7,14 @@ class FunPage extends Page {
 	
 	
 	function getCMSFields() {
-     $fields = parent::getCMSFields();
-  
-  $gridFieldConfig =$gridFieldConfig = GridFieldConfig_RecordEditor::create();
-  $gridfield = new GridField("FunThingObjects", "FunThingObject", $this->FunThingObjects, $gridFieldConfig);   
-  $fields->addFieldToTab('Root.FunThings', $gridfield);
-   
-  return $fields;
-   }
+		$fields = parent::getCMSFields();
+		
+		$gridFieldConfig = GridFieldConfig_RecordEditor::create();
+		$gridfield = new GridField("FunThingObjects", "Fun Things", $this->FunThingObjects(), $gridFieldConfig);   
+		$fields->addFieldToTab('Root.FunThings', $gridfield);
+		
+		return $fields;
+	}
 	
 }
 class FunPage_Controller extends Page_Controller {
--- themes/dragonisland/templates/Layout/FunPage.ss
+++ themes/dragonisland/templates/Layout/FunPage.ss
@@ -3,6 +3,16 @@
 	<article class="typography">
 		<h1>$Title</h1>
 		<div class="content">$Content</div>
+		
+		
+		<% loop FunThingObjects %>
+			<h3>$Title</h3>
+			$Details
+			
+		
+		
+		<% end_loop %>
+		
 	</article>
 		$Form
 		$PageComments
